c27e3120aef88ded8fec38ece8a4ae214eae2470
Sinthon <s.sinthon@cas.com.kh>
2022-11-25 13:58:49 +0700

    add app_context class

---
+++ Data/AppContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace console_entity_framwork_core.Data
+{
+    public partial class AppContext : DbContext
+    {
+        public AppContext()
+        {
+        }
+
+        public AppContext(DbContextOptions<AppContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost; Database=console_entity_framework_core; Trusted_Connection=true; MultipleActiveResultSets=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
